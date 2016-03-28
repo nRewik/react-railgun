2257ef69f46a81326ace61e21166a8334dc753ed
Nutchaphon Rewik <nrewik@outlook.com>
2016-03-28 20:43:33 +0700

    Server side hot reload 🔥

--- source/server/app.js
+++ source/server/app.js
@@ -7,17 +7,18 @@ import mainRoute from 'server/routes/main'
 export default (middlewares = []) => {
   const app = express()
 
+  // Apply other middlewares first
+  middlewares.map(middleware => {
+    app.use(middleware)
+  })
+
   const libDir = path.join(settings.APP_HOME, '/build/lib')
   app.use('/static/lib', express.static(libDir))
 
   const staticDir = path.join(settings.APP_HOME, '/static')
   app.use('/static', express.static(staticDir))
 
-  // Apply other middlewares before react-router middleware
-  middlewares.map(middleware => {
-    app.use(middleware)
-  })
-
   app.use('/', mainRoute)
+
   return app
 }
--- source/server/server.dev.js
+++ source/server/server.dev.js
@@ -1,5 +1,8 @@
-import createApp from 'server/app'
 import webpack from 'webpack'
+import chokidar from 'chokidar'
+import rimraf from 'rimraf'
+
+import createApp from 'server/app'
 import config from '../../webpack.config.dev'
 
 const NODE_PORT = process.env.NODE_PORT || 3000
@@ -7,15 +10,59 @@ const NODE_HOST = process.env.NODE_HOST || '0.0.0.0'
 
 const compiler = webpack(config)
 const middlewares = [
-  require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }),
+  require('webpack-dev-middleware')(compiler, { noInfo: true, publicPath: config.output.publicPath }),
   require('webpack-hot-middleware')(compiler)
 ]
 
 const app = createApp(middlewares)
-
 app.listen(NODE_PORT, NODE_HOST, error => {
   error ? console.error(error) : console.log(`Listening at http://${NODE_HOST}:${NODE_PORT}`)
+})
+
+// Do 'hot-reloading' of express stuff on the server
+// Throw away cached modules and re-require next time
+// Ensure there's no important state in there!
+const clearCache = () => {
+  Object.keys(require.cache).forEach(id => {
+    if (/[\/\\]build[\/\\]/.test(id)) delete require.cache[id]
+  })
+}
+
+// rebuild a babel file from ./source to ./build
+const rebuild = sourcePath => {
+  const filePath = sourcePath.split('/').slice(1).join('/')
+  const buildPath = require('path').join('build', filePath)
+  const parentDirectory = require('path').dirname(buildPath)
+  require('mkdirp')(parentDirectory, error => {
+    if (error) { console.log(error) }
+    require('child_process').exec(`babel ${sourcePath} --out-file ${buildPath} --copy-files`, (error, stdout, stderr) => {
+      if (error) { console.log(error) }
+      clearCache()
+      console.log(`rebuilt ${filePath}`)
+    })
+  })
+}
+
+// remove a file in ./build
+const remove = sourcePath => {
+  const filePath = sourcePath.split('/').slice(1).join('/')
+  const buildPath = require('path').join('build', filePath)
+  rimraf(buildPath, require('fs'), error => {
+    if (error) { console.log(error) }
+    clearCache()
+    console.log(`delete ${filePath}`)
+  })
+}
+
+// watch files in ./source
+var watcher = chokidar.watch('./source')
+watcher.on('ready', () => {
+  watcher.on('add', rebuild)
+  watcher.on('change', rebuild)
+  watcher.on('unlink', remove)
+})
+
+// clear require's cache after built webpack
+compiler.plugin('done', () => {
+  clearCache()
 })
