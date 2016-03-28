8313be04b973980b5c965b84c446b0b23344088d
Nutchaphon Rewik <nrewik@outlook.com>
2016-03-28 23:58:20 +0700

    Edit home components 😬

--- source/shared/components/home/index.js
+++ source/shared/components/home/index.js
@@ -1,23 +1,55 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
-const Home = ({ name, speed }) => {
+const Agent = ({ name, rank, onDecreaseRank, onIncreaseRank }) => {
+  return (
+    <li>
+      <button onClick={onDecreaseRank}>-</button>
+      <button onClick={onIncreaseRank}>+</button>
+      {' '}{name}—rank {rank}
+    </li>
+  )
+}
+
+Agent.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  rank: PropTypes.number.isRequired,
+  onDecreaseRank: PropTypes.func.isRequired,
+  onIncreaseRank: PropTypes.func.isRequired
+}
+
+const Home = ({ agents, onDecreaseRank, onIncreaseRank }) => {
+  const renderAgent = agent => {
+    const boundOnDecreaseRank = onDecreaseRank.bind(null, agent.id)
+    const boundOnIncreaseRank = onIncreaseRank.bind(null, agent.id)
+    return (
+      <Agent {...agent} key={agent.id} onDecreaseRank={boundOnDecreaseRank} onIncreaseRank={boundOnIncreaseRank} />
+    )
+  }
   return (
     <div>
-      <div>{name}</div>
-      <div>{speed}</div>
+      <ul>{agents.map(agent => { return renderAgent(agent) })}</ul>
     </div>
   )
 }
 
 Home.propTypes = {
-  name: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
+  agents: PropTypes.array.isRequired,
+  onDecreaseRank: PropTypes.func.isRequired,
+  onIncreaseRank: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => {
-  return { ...state.railgun }
+  return { ...state.agents }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncreaseRank: id => { dispatch({ type: 'INCREASE_RANK', id: id }) },
+    onDecreaseRank: id => { dispatch({ type: 'DECREASE_RANK', id: id }) }
+  }
 }
 
 // Connect props to component
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
